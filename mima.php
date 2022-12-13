72636564beef35fe914d3ca09cfbcffb977787f7
Milan VICENTIJEVIC <74497305+aerlaw@users.noreply.github.com>
2022-12-13 14:25:37 +0100

    Add files via upload

---
+++ mima.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
+
+    <!-- JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/1253beb9d2.js" crossorigin="anonymous"></script>
+
+    <title>Mima</title>
+</head>
+
+<body>
+    <!-- Navigation bar -->
+    <nav>
+        <a href="mima.php"><div id="mima">Mima</div></a>
+        <div class="onglets">
+            <a href="" ><div class="link">Tissus Unis</div></a>
+            <a href=""><div class="link">Tissus à Carreaux</div></a>
+            <a href=""><div class="link">Tissus à Motif</div></a>
+            <a href=""><div class="link">Nous contacter</div></a>
+        </div>
+    </nav>
+    <!-- End Navigation bar -->
+
+    <!-- Header -->
+    <header>
+        <h1>Bienvenue dans la Boutique ma, votre fournisseur de tissu de qualité</h1>
+    <!-- End Header -->
+    </header>
+
+    <!-- Card Section -->
+    <div class="card">
+        <img src="/tissus/tissu1.webp" class="card-img-top" alt="image tissu">
+        <div class="card-body">
+            <h5 class="card-title">Tissu n°1</h5>
+            <p class="card-text">Tissu de couleur en matière de ...</p>
+            <a href="#" class="btn btn-primary">Ajouter au shop</a>
+        </div>
+    </div>
+    
+    <!-- End card section-->
+
+    <!-- Footer -->
+    <footer>
+        <p>&copy; Contactez-nous au 01.43.55.76.81</p>
+        <div class="social-media">
+            <p><i class="fab fa-facebook"></i></p>
+            <a class="insta " href="https://www.instagram.com/milan_vctc "><i class="fab fa-instagram "></i></a>
+            <p><i class="fab fa-linkedin"></i></p>
+        </div>
+    </footer>
+    <!-- End Footer -->
+</body>
+
+</html>
