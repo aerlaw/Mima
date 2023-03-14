ae13197c88a22a702e364b5588539cdbc839f6f9
Milan VICENTIJEVIC <74497305+aerlaw@users.noreply.github.com>
2023-03-14 13:19:22 +0100

    Add files via upload

--- mima.php
+++ mima.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="fr" id="home">
 
 <head>
     <meta charset="UTF-8" />
@@ -20,44 +20,111 @@
 </head>
 
 <body>
-    <!-- Navigation bar -->
-    <nav>
-        <a href="mima.php"><div id="mima">Mima</div></a>
-        <div class="onglets">
-            <a href="" ><div class="link">Tissus Unis</div></a>
-            <a href=""><div class="link">Tissus à Carreaux</div></a>
-            <a href=""><div class="link">Tissus à Motif</div></a>
-            <a href=""><div class="link">Nous contacter</div></a>
-        </div>
-    </nav>
-    <!-- End Navigation bar -->
-
     <!-- Header -->
-    <header>
-        <h1>Bienvenue dans la Boutique ma, votre fournisseur de tissu de qualité</h1>
-    <!-- End Header -->
+    <div class="topnav">
+        <a href="#home" id="A">Mima</a>
+        <a href="#">Tissus Unis</a>
+        <a href="#">Tissus à Carreaux</a>
+        <a href="#">Tissus à Motif</a>
+        <a href="mailto:mima0525@orange.fr?Subject= Contacted by Mima's Website">Acheter</a>            
+    </div>
+
+    <header class="mt-5">
+        <h1 id="pres_title" class="mt-3">Bienvenue dans la Boutique Mima, votre fournisseur de tissu de qualité</h1>
+        <div id="header_grid" class="mt-5">
+            <img id="pres_img" src="img/pres.webp" alt="image d'une machine à coudre">
+            <div id="pres_txt">Créer en 2004, l'atelier Mima est une institution pour tout créateur de vêtements, designeur, styliste qui se veut être à la mode. Depuis son lancement, Julijana, la gérante, se démène pour fournir tout le temps une qualité irréprochable à ses clients afin de garentir la qualité Made In France que leurs clients aiment tant. L'atelier Mima à vu et à aider à batir les plus grands noms de la mode d'aujourd'hui tels que: Jacquemus, Marie Labarelle, Barbara Lang et tant d'autres.</div>
+        </div>
     </header>
 
-    <!-- Card Section -->
-    <div class="card">
-        <img src="/tissus/tissu1.webp" class="card-img-top" alt="image tissu">
-        <div class="card-body">
-            <h5 class="card-title">Tissu n°1</h5>
-            <p class="card-text">Tissu de couleur en matière de ...</p>
-            <a href="#" class="btn btn-primary">Ajouter au shop</a>
+    <!-- End Header -->
+
+    <!-- Slideshow -->
+
+    <div id="carouselExampleCaptions" class="carousel slide mt-3" data-bs-ride="carousel">
+                <div class="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
+                </div>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="img/jacquemus.png" class="d-block w-100 ss_img" alt="slideshow 1 | Jacquemus">
+                        <div class="carousel-caption d-none d-md-block mt-5">
+                            <p>Started in 2011</p>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="img/elsa.png" class="d-block w-100 ss_img" alt="slideshow 2 | Elsa Esturgie">
+                        <div class="carousel-caption d-none d-md-block">
+                            <p>Started in 2009</p>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="img/barbara.png" class="d-block w-100 ss_img" alt="slideshow 3 | Barbara Lang">
+                        <div class="carousel-caption d-none d-md-block">
+                            <p>Started in 2007</p>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="img/adn.png" class="d-block w-100 ss_img" alt="slideshow 4 | ADN">
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+
+    <!-- End Header -->
+    <div class="title">Nos tissus</div>
+    <div class="tissu">
+    <?php 
+        include 'connexion.php';
+        $stmt = $conn->prepare('SELECT * FROM mima order by id asc');
+        $stmt->execute();
+        if($stmt->rowCount() > 0) {
+            while ($row=$stmt->fetch(PDO::FETCH_ASSOC)) {
+                extract($row); ?>
+        <!-- Card Section -->
+        <div class="card">
+            <div class="card-inner">
+                <img src="tissu/<?php echo $row['file']?>" class="card-img-top" alt="image tissu">
+                <div class="card-body">
+                    <p><?php echo $metrage ?> m</p>
+                    <h5 class="card-title"><?php echo $name ?></h5>
+                    <p class="card-text"><?php echo $description ?></p>
+                    <button class="btn btn-primary bouton">Afficher en détails</button>
+                </div>
+            </div>
         </div>
+    <?php
+            }
+        }
+    ?>
     </div>
-    
     <!-- End card section-->
 
     <!-- Footer -->
     <footer>
-        <p>&copy; Contactez-nous au 01.43.55.76.81</p>
-        <div class="social-media">
-            <p><i class="fab fa-facebook"></i></p>
-            <a class="insta " href="https://www.instagram.com/milan_vctc "><i class="fab fa-instagram "></i></a>
-            <p><i class="fab fa-linkedin"></i></p>
+        <div class="footer-content">
+            <h3>Mima Development</h3>
+            <ul class="socials">
+                <li><a href="https://www.instagram.com/latelier.julie/?hl=fr"><i class="fa fa-instagram"></i></a></li>
+                <li><a href="https://www.facebook.com/julijana.vicentijevic"><i class="fa fa-facebook"></i></a></li>
+                <li><a href="mailto:mima0525@orange.fr?Subject= Contacted by Mima's Website"><i class="fa fa-envelope"></i></a></li>
+            </ul>
         </div>
+        <div class="footer-bottom">
+            <div class="footer_end text-center"><a href="https://github.com/aerlaw">Milan V. | Aerlaw</a></div>
+            <div class="footer_end text-center">&copy; 2023 - All rights reserved</div>
+        </div>
+
     </footer>
     <!-- End Footer -->
 </body>
